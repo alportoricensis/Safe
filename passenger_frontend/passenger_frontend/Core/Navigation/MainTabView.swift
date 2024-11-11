38742b7a3652e37f1740f7b3de0e1649194efe3b
Kunal Mansukhani <kmmansukhani@gmail.com>
2024-11-11 11:14:43 -0500

    fixes

--- passenger_frontend/passenger_frontend/Core/Navigation/MainTabView.swift
+++ passenger_frontend/passenger_frontend/Core/Navigation/MainTabView.swift
@@ -4,7 +4,6 @@ struct MainTabView: View {
     @EnvironmentObject var authViewModel: AuthViewModel
     @State private var selectedTab: Tab = .services
     
-    // Define constants for colors
     private let backgroundColor = Color(red: 0/255, green: 39/255, blue: 76/255)
     private let selectedColor = Color(red: 255/255, green: 203/255, blue: 5/255) // Yellow
     private let unselectedColor = Color.white
@@ -46,20 +45,16 @@ struct MainTabView: View {
                 .tag(Tab.account)
         }
         .onAppear {
-            // Set the tab bar background color
             let appearance = UITabBarAppearance()
             appearance.configureWithOpaqueBackground()
             appearance.backgroundColor = UIColor(backgroundColor)
             
-            // Set the unselected item color
             appearance.stackedLayoutAppearance.normal.iconColor = UIColor(unselectedColor)
             appearance.stackedLayoutAppearance.normal.titleTextAttributes = [.foregroundColor: UIColor(unselectedColor)]
             
-            // Set the selected item color
             appearance.stackedLayoutAppearance.selected.iconColor = UIColor(selectedColor)
             appearance.stackedLayoutAppearance.selected.titleTextAttributes = [.foregroundColor: UIColor(selectedColor)]
             
-            // Apply the appearance
             UITabBar.appearance().standardAppearance = appearance
             if #available(iOS 15.0, *) {
                 UITabBar.appearance().scrollEdgeAppearance = appearance
