61535a587307400c1267991a360e249b518ff6c4
Alex Gabriel Nunez-Carrasquillo <123135744+alportoricensis@users.noreply.github.com>
2024-12-02 14:04:33 -0500

    Added booking future rides

--- Safe_backend/safe_backend/api/config.py
+++ Safe_backend/safe_backend/api/config.py
@@ -1,7 +1,11 @@
 """Global variables and configurations for Safe's backend"""
+from apscheduler.schedulers.background import BackgroundScheduler
 
 # Globals
 # VEHICLE_QUEUES maps a vehicle_id to the vehicle object.
 VEHICLES = {}
 # RIDE_REQUESTS is a dictionary of all currenty active ride ride requests
 REQUESTS = {}
+# Scheduler for ride requests
+scheduler = BackgroundScheduler()
+scheduler.start()
