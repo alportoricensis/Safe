de7c7098dfe520efcb76a764bbba3435f9fa85a0
TeageJ <teagej@umich.edu>
2024-12-03 14:15:17 -0500

    dispatcher updates

--- dispatcher_frontend/src/Components/Pages/Settings.jsx
+++ dispatcher_frontend/src/Components/Pages/Settings.jsx
@@ -3,33 +3,33 @@ import {Box, Link, Typography} from '@mui/material';
 
 const Settings = () => {
     return (
-        <Box style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', flexWrap: 'wrap', width: '90vw', height: '100vh'}}>
-            <Box href='/settings/services' component={Link} style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', width: '25vw', height: '40vh', textDecoration: 'none', color: 'black'}} sx={{border: 1, borderRadius: 10}}>
+        <Box style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center', flexWrap: 'wrap', width: '90vw', height: '100vh', backgroundColor: 'rgb(3, 38, 72)'}}>
+            <Box href='/settings/services' component={Link} style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', width: '25vw', height: '35vh', textDecoration: 'none', color: 'black', backgroundColor: 'white'}} sx={{borderRadius: 10}}>
                 <Typography fontSize={'3em'}>
                     Services
                 </Typography>
             </Box>
-            <Box href='/settings/ranges' component={Link} style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', width: '25vw', height: '40vh', textDecoration: 'none', color: 'black'}} sx={{border: 1, borderRadius: 10}}>
+            <Box href='/settings/ranges' component={Link} style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', width: '25vw', height: '35vh', textDecoration: 'none', color: 'black', backgroundColor: 'white'}} sx={{borderRadius: 10}}>
                 <Typography fontSize={'3em'}>
                     Service Ranges
                 </Typography>
             </Box>
-            <Box href='/settings/locations' component={Link} style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', width: '25vw', height: '40vh', textDecoration: 'none', color: 'black'}} sx={{border: 1, borderRadius: 10}}>
+            <Box href='/settings/locations' component={Link} style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', width: '25vw', height: '35vh', textDecoration: 'none', color: 'black', backgroundColor: 'white'}} sx={{borderRadius: 10}}>
                 <Typography fontSize={'3em'}>
                     Service Locations
                 </Typography>
             </Box>
-            <Box href='/settings/times' component={Link} style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', width: '25vw', height: '40vh', textDecoration: 'none', color: 'black'}} sx={{border: 1, borderRadius: 10}}>
+            <Box href='/settings/times' component={Link} style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', width: '25vw', height: '35vh', textDecoration: 'none', color: 'black', backgroundColor: 'white'}} sx={{borderRadius: 10}}>
                 <Typography fontSize={'3em'}>
                     Service Times
                 </Typography>
             </Box>
-            <Box href='/settings/vehicles' component={Link} style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', width: '25vw', height: '40vh', textDecoration: 'none', color: 'black'}} sx={{border: 1, borderRadius: 10}}>
+            <Box href='/settings/vehicles' component={Link} style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', width: '25vw', height: '35vh', textDecoration: 'none', color: 'black', backgroundColor: 'white'}} sx={{borderRadius: 10}}>
                 <Typography fontSize={'3em'}>
                     Register Vehicles
                 </Typography>
             </Box>
-            <Box href='/settings/routing' component={Link} style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', width: '25vw', height: '40vh', textDecoration: 'none', color: 'black'}} sx={{border: 1, borderRadius: 10}}>
+            <Box href='/settings/routing' component={Link} style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center', width: '25vw', height: '35vh', textDecoration: 'none', color: 'black', backgroundColor: 'white'}} sx={{borderRadius: 10}}>
                 <Typography fontSize={'3em'}>
                     Routing Options
                 </Typography>
